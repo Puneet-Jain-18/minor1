b25ff723c47d3ac111b4a04107e0156351c5c299
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-08-20 11:59:26 +0530

    nodejs tensor

---
+++ tensor.js
@@ -0,0 +1,84 @@
+var posenet=require('@tensorflow-models/posenet');
+const tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node');
+const coverFolder = './data/training/cover';
+const cutFolder = './data/training/cut';
+const pullFolder = './data/training/pull';
+const scoopFolder = './data/training/scoop';
+const straightFolder = './data/training/straight';
+const legFolder = './data/training/leg';
+const fs = require('fs');
+const { createCanvas, Image } = require('canvas');
+const canvas = createCanvas(200, 200);
+
+const ctx = canvas.getContext('2d');
+
+
+const img = new Image();
+img.src = '/home/puneet/Work/minor1/data/training/scoop/100_scoop_main.jpg';
+  setTimeout(async function(){
+    ctx.drawImage(img, 0, 0);
+
+  const input =tf.browser.fromPixels(canvas);
+
+  singlePoseOutput(input).then((data)=>
+  {
+    console.log(data);
+  })
+  },
+5000);
+
+
+async function singlePoseOutput(imageElement) {
+  const imageScaleFactor = 0.50;
+  const flipHorizontal = false;
+  const outputStride = 16;
+
+  const net = await posenet.load();
+
+  const pose = await net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride);
+  return pose;
+};
+
+// arr=['cover','cut','leg',"pull","scoop","straight"]
+// list={}
+// for (i=0;i<6;i++)
+// {
+//   testFolder="./data/training/"+arr[i];
+//   final=[];
+//   fs.readdir(testFolder, (err, files) => {
+//     files.forEach(file => {
+//       f=testFolder+"/"+file
+//       abc(f).then((data)=>
+//       {
+//         //console.log(data)
+//         //console.log("Returned from function")
+//       }).catch((err)=>
+//       {
+//         console.log(err)
+//       })
+
+//     });
+//   });
+// }
+
+// async function abc(file)
+// {
+
+// const img = new Image();
+// img.src = file;
+//   setTimeout(async function(){
+//     ctx.drawImage(img, 0, 0).then(()=>{
+//       const input =tf.browser.fromPixels(canvas);
+
+//   singlePoseOutput(input).then((data)=>
+//   {
+//     console.log(data);
+//   })
+//     })
+//   },
+// 5000);
+//   }
+
+
+
