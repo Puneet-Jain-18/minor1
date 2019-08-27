c498096ec71bf2bef9a5ce8772bdd3cf33839bd2
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-08-27 18:54:44 +0530

    Logistic regression(Might be wrong)

--- tensor.js
+++ tensor.js
@@ -1,60 +1,33 @@
 var posenet=require('@tensorflow-models/posenet');
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
-const coverFolder = './data/training/cover';
-const cutFolder = './data/training/cut';
-const pullFolder = './data/training/pull';
-const scoopFolder = './data/training/scoop';
-const straightFolder = './data/training/straight';
-const legFolder = './data/training/leg';
 const fs = require('fs');
 const { createCanvas, Image } = require('canvas');
 
 
-
-const img = new Image();
-img.src = '/home/puneet/Work/minor1/data/training/scoop/100_scoop_main.jpg';
-
-console.log(img)
-
-const canvas = createCanvas(200, 300);
-
-const ctx = canvas.getContext('2d');
-  setTimeout(async function(){
-    ctx.drawImage(img, 0, 0,img.width,img.height);
-
-  const input =tf.browser.fromPixels(canvas);
-
-  singlePoseOutput(input).then((data)=>
-  {
-    console.log(data);
-  })
-  },
-1000);
-
-
 async function singlePoseOutput(imageElement) {
-  const imageScaleFactor = 0.50;
-  const flipHorizontal = false;
-  const outputStride = 16;
+  var imageScaleFactor = 0.50;
+  var flipHorizontal = false;
+  var outputStride = 16;
 
-  const net = await posenet.load();
+  var net = await posenet.load();
 
-  const pose = await net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride);
+  var pose = await net.estimateSinglePose(imageElement, imageScaleFactor, flipHorizontal, outputStride);
   return pose;
 };
 
 arr=['cover','cut','leg',"pull","scoop","straight"]
 list={}
-for (i=0;i<6;i++)
-{
-  testFolder="./data/training/"+arr[i];
+//for (i=0;i<6;i++)
+//{
+  testFolder="./data/training/"+arr[0];
   final=[];
   fs.readdir(testFolder, (err, files) => {
     files.forEach(file => {
       f=testFolder+"/"+file
-      abc(f).then((data)=>
+      abc(f,file).then((data)=>
       {
+        
       }).catch((err)=>
       {
         console.log(err)
@@ -62,32 +35,49 @@ for (i=0;i<6;i++)
 
     });
   });
-}
+//}
 
-async function abc(file)
-{
-  const img = new Image();
+async function abc(file ,f1)
+{console.log("AAAA")
+  var img = new Image();
     
-  const canvas = createCanvas(200, 300);
+  var canvas = createCanvas(200, 300);
   
-  const ctx = canvas.getContext('2d');
+  var ctx = canvas.getContext('2d');
   img.onload=()=>{
     ctx.drawImage(img, 0, 0,img.width,img.height)
   
-    const input =tf.browser.fromPixels(canvas);
-  
+    var input =tf.browser.fromPixels(canvas);
     singlePoseOutput(input).then((data)=>
     {
+      console.log("H")
       console.log(data);
+      var str=data.score+","+data.keypoints[0].position.x+","+data.keypoints[0].position.y+","+data.keypoints[0].score+","+
+              data.keypoints[1].position.x+","+data.keypoints[1].position.y+","+data.keypoints[1].score+","+
+              data.keypoints[2].position.x+","+data.keypoints[2].position.y+","+data.keypoints[2].score+","+
+              data.keypoints[3].position.x+","+data.keypoints[3].position.y+","+data.keypoints[3].score+","+
+              data.keypoints[4].position.x+","+data.keypoints[4].position.y+","+data.keypoints[4].score+","+
+              data.keypoints[5].position.x+","+data.keypoints[5].position.y+","+data.keypoints[5].score+","+
+              data.keypoints[6].position.x+","+data.keypoints[6].position.y+","+data.keypoints[6].score+","+
+              data.keypoints[7].position.x+","+data.keypoints[7].position.y+","+data.keypoints[7].score+","+
+              data.keypoints[8].position.x+","+data.keypoints[8].position.y+","+data.keypoints[8].score+","+
+              data.keypoints[9].position.x+","+data.keypoints[9].position.y+","+data.keypoints[9].score+","+
+              data.keypoints[10].position.x+","+data.keypoints[10].position.y+","+data.keypoints[10].score+","+
+              data.keypoints[11].position.x+","+data.keypoints[11].position.y+","+data.keypoints[11].score+","+
+              data.keypoints[12].position.x+","+data.keypoints[12].position.y+","+data.keypoints[12].score+","+
+              data.keypoints[13].position.x+","+data.keypoints[13].position.y+","+data.keypoints[13].score+","+
+              data.keypoints[14].position.x+","+data.keypoints[14].position.y+","+data.keypoints[14].score+","+
+              data.keypoints[15].position.x+","+data.keypoints[15].position.y+","+data.keypoints[15].score+","+
+              data.keypoints[16].position.x+","+data.keypoints[16].position.y+","+data.keypoints[16].score+","+f1+",straight\n";
+              console.log(str);
+      fs.appendFile("cover.txt",str, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+      })
 
     }).catch((msg)=>{
       console.log(msg)
     })
   }
   img.src = file;
-
  }
-
-
-
-
